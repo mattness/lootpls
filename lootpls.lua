c1197acad99996888ddc45e86fb6b19f3046b541
MattnessX <mattnessx@gmail.com>
2010-12-06 23:18:00 -0500

    oops, 'git commit -a' didn't quite work how I thought it would... adding missed file, lootpls.lua

---
+++ lootpls.lua
@@ -0,0 +1,92 @@
+local addonName = "LootPls";
+LootPls = LibStub("AceAddon-3.0"):NewAddon(addonName, "AceConsole-3.0", "AceEvent-3.0", "AceComm-3.0", "AceSerializer-3.0");
+_G[addonName] = LootPls;
+local addon = LootPls;
+
+local AddonDB_Defaults = {
+	global = {
+		Options = {
+		},
+	},
+};
+
+local dummyData = {
+	Guilds = {
+		['*'] = {
+			Raiders = {
+				['*'] = {
+					class = nil,
+					data1 = nil,
+					data2 = nil,
+				},
+			},
+		},
+	},
+	Characters = {
+		['*'] = {
+			class = nil,
+			Guilds = {
+				['*'] = {
+					data1 = nil,
+					data2 = nil,
+				},
+			},
+		},
+	},
+};
+
+local function GetOption(option)
+	return addon.db.global.Options[option]
+end
+
+function addon:OnInitialize()
+	addon.db = LibStub("AceDB-3.0"):New(addonName .. "DB", AddonDB_Defaults);
+	SetupDummyData();
+	setmetatable(LootPls, { __index = addon });
+end
+
+function addon:GetMemberInfo(guildKey, index)
+	-- This is a hack to get the member name from the index.  It needs to be reworked.
+	local name = GetGuildRosterInfo(index);
+	if ( name == nil ) then return nil end
+	
+	local member = dummyData.Guilds[guildKey].Raiders[name];
+	return name, member.class, false, member.data1, member.data2, member.data1/member.data2;
+end
+
+local guildKey = format("%s.%s", GetRealmName(), GetGuildInfo("player"));
+function addon:GetMemberCount(realm, guild)
+	local guildTable = dummyData.Guilds[guildKey];
+	if ( guildTable == nil ) then return 0 end
+
+	local count = 0
+	for _, _ in pairs(guildTable.Raiders) do
+		count = count + 1
+	end
+
+	return count;
+end
+
+-- This is a throw-away function just to generate some static data for developing and testing the sorting story.
+function SetupDummyData()
+	local recordCount = GetNumGuildMembers();
+	dummyData.Guilds[guildKey] = { Raiders = { } };
+	local guildTable = dummyData.Guilds[guildKey];
+	local charactersTable = dummyData.Characters;
+	
+	for i = 1, recordCount do
+		local name, _, _, _, _, _, _, _, online, _, classFileName = GetGuildRosterInfo(i);
+		local ep = math.random(1000, 10000);
+		local gp = math.random(100, 1000);
+		local characterKey = format("%s.%s", GetRealmName(), name);
+		
+		charactersTable[characterKey] = {
+			class = classFileName,
+			Guilds = {
+				[guildKey] = { data1 = ep, data2 = gp },
+			}
+		};
+
+		guildTable.Raiders[name] = { class = classFileName, data1 = ep, data2 = gp };
+	end
+end
